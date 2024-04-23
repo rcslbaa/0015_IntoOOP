baf7e90119edf34be6b95b989e3714257147620e
rcslbaa <ifasmarani3125@gmail.com>
2024-04-23 19:39:39 +0700

    06 input kode mk. nama mk, sks

--- PertamuanKeTujuh/PertamuanKeTujuh.cpp
+++ PertamuanKeTujuh/PertamuanKeTujuh.cpp
@@ -19,3 +19,12 @@ private:
 	string kodeMk;
 	string namaMk;
 	int sks;
+public:
+	void input() {
+		cout << "Kode MK: ";
+		cin >> kodeMk;
+		cout << "Nama MK: ";
+		cin >> namaMk;
+		cout << "SKS: ";
+		cin >> sks;
+	}
