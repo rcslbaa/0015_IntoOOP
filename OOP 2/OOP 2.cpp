4b5f863cbb4dde583b6da9df8a71cd3d74f92703
rcslbaa <ifasmarani3125@gmail.com>
2024-04-23 19:43:34 +0700

    11 bangun datar

--- OOP 2/OOP 2.cpp
+++ OOP 2/OOP 2.cpp
@@ -2,36 +2,3 @@
 using namespace std;
 
 class bangunDatar {
-
-	//akses modifier
-private:
-	float panjang, Lebar;
-public:
-	float Luas;
-
-	void input() { //methode input persegi panjang
-		
-		cout << "Masukan Panjangnya = ";
-		cin >> panjang;
-		cout << "Masukan Lebarnya = ";
-		cin >> Lebar;
-	}
-
-	float hitungLuas() {
-		return panjang * Lebar;
-	}
-
-	void display() {
-		cout << "Panjangnya = " << panjang << endl;
-		cout << "Lebarnya = " << Lebar << endl;
-		cout << "Luasnya = " << hitungLuas() << endl;
-	}
-};
-
-int main() {
-	bangunDatar pp; //deklarasi objek pp dari class bangunDatar
-	pp.input();
-	pp.display();
-
-	return 0;
-}
