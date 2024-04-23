3d26deadfae709cecd25617593ea7f58c3eb3e3c
rcslbaa <ifasmarani3125@gmail.com>
2024-04-23 19:09:02 +0700

    Add project files.

---
+++ OOP 2/OOP 2.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+class bangunDatar {
+
+	//akses modifier
+private:
+	float panjang, Lebar;
+public:
+	float Luas;
+
+	void input() { //methode input persegi panjang
+		
+		cout << "Masukan Panjangnya = ";
+		cin >> panjang;
+		cout << "Masukan Lebarnya = ";
+		cin >> Lebar;
+	}
+
+	float hitungLuas() {
+		return panjang * Lebar;
+	}
+
+	void display() {
+		cout << "Panjangnya = " << panjang << endl;
+		cout << "Lebarnya = " << Lebar << endl;
+		cout << "Luasnya = " << hitungLuas() << endl;
+	}
+};
+
+int main() {
+	bangunDatar pp; //deklarasi objek pp dari class bangunDatar
+	pp.input();
+	pp.display();
+
+	return 0;
+}
